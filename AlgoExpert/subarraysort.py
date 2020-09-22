8bcd2c70cbfe0c58873652d1085590026a90ce9a
Justin Lowen <Justin.G.Lowen@gmail.com>
2020-09-22 14:35:26 -0500

    Refactor and clean up subarray sort algorithm

--- AlgoExpert/subarraysort.py
+++ AlgoExpert/subarraysort.py
@@ -1,24 +1,30 @@
 # Given an array, return the indexes of the smallest subarray that can be
 # sorted to sort the overall array.
 
+# Time Complexity O(N) - consecutive single loop operations through N elements
+# Space Complexity O(1) - no additional storage that scales with input size
+
 def subarraySort(array):
-    subarrayIdx = [-1, -1]
-    smallestUnsorted = float("inf")
-    largestUnsorted = float("-inf")
-    for i in range(len(array) - 1):
-        if array[i + 1] < array[i]:
-            if array[i + 1] < smallestUnsorted:
-                smallestUnsorted = array[i + 1]
-            if array[i] < smallestUnsorted:
-                smallestUnsorted = array[i]
-        if array[i + 1] < array[i]:
-            if array[i + 1] > largestUnsorted:
-                largestUnsorted = array[i + 1]
-            if array[i] > largestUnsorted:
-                largestUnsorted = array[i]
+    minOutOfOrder = float("inf")
+    maxOutOfOrder = float("-inf")
     for i in range(len(array)):
-        if array[i] > smallestUnsorted and subarrayIdx[0] == -1:
-            subarrayIdx[0] =  i
-        if array[len(array) - 1 - i] < largestUnsorted and subarrayIdx[1] == -1:
-            subarrayIdx[1] = len(array) - 1 - i
-    return subarrayIdx
+        num = array[i]
+        if isOutOfOrder(i, num, array):
+            minOutOfOrder = min(minOutOfOrder, num)
+            maxOutOfOrder = max(maxOutOfOrder, num)
+    if minOutOfOrder == float("inf"):
+        return [-1, -1]
+    subarrayLeftIdx = 0
+    while minOutOfOrder >= array[subarrayLeftIdx]:
+        subarrayLeftIdx += 1
+    subarrayRightIdx = len(array) - 1
+    while maxOutOfOrder <= array[subarrayRightIdx]:
+        subarrayRightIdx -= 1
+    return [subarrayLeftIdx, subarrayRightIdx]
+
+def isOutOfOrder(i, num, array):
+    if i == 0:
+        return num > array[i + 1]
+    if i == len(array) - 1:
+        return num < array[i - 1]
+    return num > array[i + 1] or num < array[i - 1]
