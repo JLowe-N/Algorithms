4725b90bca877ae20838132d80801f895956075d
Justin Lowen <Justin.G.Lowen@gmail.com>
2020-10-19 12:57:09 -0500

    Implement merge sort algorithm

---
+++ AlgoExpert/mergesort.py
@@ -0,0 +1,73 @@
+# Pointer Solution
+# Complexity O(N log N) Time | O(N) Space where N is the number of elements to sort
+# This solution uses an auxiliary array which is a copy of the input array of length N
+# This avoids potential O(N log N) complexity from passing multiple subarray copies through
+# the recursion call stack where log N levels of N element copies may exist.
+def mergeSort(array):
+    if len(array) <= 1:
+        return array
+    auxiliaryArray = array[:]
+    mergeSortHelper(array, 0, len(array) - 1, auxiliaryArray)
+    return array
+
+
+def mergeSortHelper(mainArray, startIdx, endIdx, auxiliaryArray):
+    if startIdx == endIdx:
+        return
+    middleIdx = (endIdx + startIdx) // 2
+    mergeSortHelper(auxiliaryArray, startIdx, middleIdx, mainArray)
+    mergeSortHelper(auxiliaryArray, middleIdx + 1, endIdx, mainArray)
+    doMerge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray)
+
+
+def doMerge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray):
+    k = startIdx
+    i = startIdx
+    j = middleIdx + 1
+    while i <= middleIdx and j <= endIdx:
+        if auxiliaryArray[i] <= auxiliaryArray[j]:
+            mainArray[k] = auxiliaryArray[i]
+            i += 1
+        else:
+            mainArray[k] = auxiliaryArray[j]
+            j += 1
+        k += 1
+    while i <= middleIdx:
+        mainArray[k] = auxiliaryArray[i]
+        i += 1
+        k += 1
+    while j <= endIdx:
+        mainArray[k] = auxiliaryArray[j]
+        j += 1
+        k += 1
+
+# Solution 2 - recursion with copies of the array rather than pointers
+# Complexity O(N log N) time | O(N log N) space
+# def mergeSort(array):
+#     if len(array) <= 1:
+#         return array
+#     middleIdx = len(array) // 2
+#     leftHalf = array[:middleIdx]
+#     rightHalf = array[middleIdx:]
+#     return mergeSortedArrays(mergeSort(leftHalf), mergeSort(rightHalf))
+
+# def mergeSortedArrays(leftHalf, rightHalf):
+#     sortedArray = [None] * (len(leftHalf) + len(rightHalf))
+#     k = i = j = 0
+#     while i < len(leftHalf) and j < len(rightHalf):
+#         if leftHalf[i] <= rightHalf[j]:
+#             sortedArray[k] = leftHalf[i]
+#             i += 1
+#         else:
+#             sortedArray[k] = rightHalf[j]
+#             j += 1
+#         k += 1
+#     while i < len(leftHalf):
+#         sortedArray[k] = leftHalf[i]
+#         i += 1
+#         k += 1
+#     while j < len(rightHalf):
+#         sortedArray[k] = rightHalf[j]
+#         j += 1
+#         k += 1
+#     return sortedArray
---
+++ testing/testMergeSort.py
@@ -0,0 +1,8 @@
+from AlgoExpert import mergesort as program
+import unittest
+
+
+class TestProgram(unittest.TestCase):
+    def test_case_1(self):
+        self.assertEqual(program.mergeSort([8, 5, 2, 9, 5, 6, 3]), [
+                         2, 3, 5, 5, 6, 8, 9])
